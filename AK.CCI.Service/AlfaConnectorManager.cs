ff71711e7f6c624c72987eedcc5a4140ceed611e
Shadance <alex@hausnet.ru>
2016-02-19 14:46:09 +0300

    new files

---
+++ AK.CCI.Service/AlfaConnectorManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Linq;
+using System.Net;
+using System.Security;
+using System.Threading;
+using AK.CCI.Service.Settings;
+using Ecng.Common;
+using log4net;
+using StockSharp.Algo.Candles;
+using StockSharp.BusinessEntities;
+using StockSharp.AlfaDirect;
+using StockSharp.Algo;
+using StockSharp.Algo.Testing;
+using StockSharp.Localization;
+using StockSharp.Logging;
+using StockSharp.Messages;
+using LogManager = log4net.LogManager;
+
+namespace AK.CCI.Service
+{
+	public class AlfaConnectorManager : IConnectorManager
+	{
+		protected static readonly ILog Log = LogManager.GetLogger("AK.CCI.Service");
+		protected IConfiguration _configuration;
+
+		protected AlfaTrader _trader;
+		protected CandleManager _candleManager;
+	    protected Security _security;
+        
+		public ManualResetEvent TraderConnectedEvent { get; } = new ManualResetEvent(false);
+
+        public AlfaConnectorManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public virtual IConnector Trader
+		{
+			get
+			{
+				if (_trader == null)
+				{
+                    _trader = new AlfaTrader { LogLevel = LogLevels.Debug };
+
+                    _trader.Restored += () => Log.Info("Connection to AlfaDirect restored.");
+                    _trader.NewSecurities += securities => Log.Info("Trader.NewSecurities");
+                    _trader.NewSecurities += securities =>
+                    {
+                        if (securities.All(s => s != _security))
+                            return;
+
+                        var level1Info = new Level1ChangeMessage
+                        {
+                            SecurityId = _security.ToSecurityId(),
+                        };
+                        // fill level1 values
+                        _trader.SendInMessage(level1Info);
+                        _trader.RegisterTrades(_security);
+                        _trader.RegisterMarketDepth(_security);
+                    };
+
+                    // подписываемся на событие успешного соединения
+                    _trader.Connected += () => TraderConnectedEvent.Set();
+
+                    // подписываемся на событие успешного отключения
+                    _trader.Disconnected += () => TraderConnectedEvent.Reset();
+
+                    // подписываемся на событие разрыва соединения
+                    _trader.ConnectionError += error => Log.Info("Connection to AlfaDirect lost!");
+
+                    // подписываемся на ошибку обработки данных (транзакций и маркет)
+                    _trader.Error += error => Log.Error(error.ToString());
+
+                    // подписываемся на ошибку подписки маркет-данных
+                    _trader.MarketDataSubscriptionFailed += (security, type, error) =>
+                        Log.Error(error.ToString() + LocalizedStrings.Str2956Params.Put(type, security));
+
+//                    _trader.StateChanged += () => Log.InfoFormat("Trader.StateChanged {0}", _trader.State);
+
+                    // подписываемся на событие о неудачной регистрации заявок
+                    _trader.OrdersRegisterFailed += fails =>
+				    {
+				        foreach (var fail in fails)
+				            Log.Error(fail.Error);
+				    };
+                    // подписываемся на событие о неудачном снятии заявок
+                    _trader.OrdersCancelFailed += fails =>
+                    {
+                        foreach (var fail in fails)
+                            Log.Error(fail.Error);
+                    };
+                    // подписываемся на событие о неудачной регистрации стоп-заявок
+                    _trader.StopOrdersRegisterFailed += fails =>
+                    {
+                        foreach (var fail in fails)
+                            Log.Error(fail.Error);
+                    };
+                    // подписываемся на событие о неудачном снятии стоп-заявок
+                    _trader.StopOrdersCancelFailed += fails =>
+                    {
+                        foreach (var fail in fails)
+                            Log.Error(fail.Error);
+                    };
+
+				    _trader.Login = _configuration.Login;
+                    _trader.Password = _configuration.Password;
+                }
+
+                if ((_trader.ConnectionState == ConnectionStates.Disconnected)|| (_trader.ConnectionState == ConnectionStates.Failed))
+                    _trader.Connect();
+
+				return _trader;
+			}
+		}
+
+		public CandleManager CandleManager
+		{
+			get
+			{
+				if (_candleManager == null)
+				{
+					_candleManager = new CandleManager(_trader);
+					_candleManager.Error += exception => Log.Error("CandleManager.Error", exception);
+				}
+
+				return _candleManager;
+			}
+		}
+
+    }
+}
---
+++ AK.CCI.Service/Strategies/AKStrategy.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AK.CCI.Service.Settings;
+using Ecng.Common;
+using log4net;
+using log4net.Appender;
+using StockSharp.Algo;
+using StockSharp.Algo.Candles;
+using StockSharp.Algo.Risk;
+using StockSharp.Algo.Strategies;
+using StockSharp.BusinessEntities;
+using StockSharp.Logging;
+using StockSharp.Messages;
+using StockSharp.Quik;
+using StockSharp.Xaml;
+using LogManager = log4net.LogManager;
+
+namespace AK.CCI.Service.Strategies
+{
+    public class AKStrategy : Strategy, IStrategyConfiguration
+    {
+        protected static readonly ILog Logger = LogManager.GetLogger("AK.CCI.Service");
+      
+        //        private ChartArea _area;
+        //        protected Portfolio _portfolio;
+        protected CandleSeries Series;
+
+        protected ManualResetEvent PortfolioFoundEvent  = new ManualResetEvent(false);
+        protected ManualResetEvent SecurityFoundEvent   = new ManualResetEvent(false);
+
+        protected Timer OrdersCheckTimer;
+
+        protected readonly StrategyParam<TimeSpan> _CandleTimeFrame;
+        [DisplayName("CandleTimeFrame")]
+        public TimeSpan CandleTimeFrame
+        {
+            get { return _CandleTimeFrame.Value; }
+            set { _CandleTimeFrame.Value = value; }
+        }
+
+        protected readonly StrategyParam<string> _PortfolioName;
+        [DisplayName("PortfolioName")]
+        public string PortfolioName
+        {
+            get { return _PortfolioName.Value; }
+            set { _PortfolioName.Value = value; }
+        }
+
+        protected readonly StrategyParam<int> _IndicatorLength;
+        [DisplayName("Indicator Length")]
+        public int IndicatorLength
+        {
+            get { return _IndicatorLength.Value; }
+            set { _IndicatorLength.Value = value; }
+        }
+
+        protected readonly StrategyParam<string> _SecurityCode;
+        [DisplayName("Security Code")]
+        public string SecurityCode
+        {
+            get { return _SecurityCode.Value; }
+            set { _SecurityCode.Value = value; }
+        }
+
+        protected readonly StrategyParam<TimeSpan> _OrdersCheckInterval;
+        [DisplayName("OrdersCheckInterval")]
+        public TimeSpan OrdersCheckInterval
+        {
+            get { return _OrdersCheckInterval.Value; }
+            set { _OrdersCheckInterval.Value = value; }
+        }
+
+        protected readonly StrategyParam<TimeSpan> _OrderExpirationTimeSpan;
+        [DisplayName("OrderExpirationTimeSpan")]
+        public TimeSpan OrderExpirationTimeSpan
+        {
+            get { return _OrderExpirationTimeSpan.Value; }
+            set { _OrderExpirationTimeSpan.Value = value; }
+        }
+
+        protected readonly StrategyParam<int> _StopLossLevel;
+        [DisplayName("StopLossLevel")]
+        public int StopLossLevel
+        {
+            get { return _StopLossLevel.Value; }
+            set { _StopLossLevel.Value = value; }
+        }
+
+        protected readonly StrategyParam<int> _TakeProfitLevel;
+        [DisplayName("TakeProfitLevel")]
+        public int TakeProfitLevel
+        {
+            get { return _TakeProfitLevel.Value; }
+            set { _TakeProfitLevel.Value = value; }
+        }
+
+        protected readonly StrategyParam<int> _TakeProfitOffset;
+        [DisplayName("TakeProfitOffset")]
+        public int TakeProfitOffset
+        {
+            get { return _TakeProfitOffset.Value; }
+            set { _TakeProfitOffset.Value = value; }
+        }
+
+        public AKStrategy()
+        {
+            _CandleTimeFrame = this.Param("CandleTimeFrame", TimeSpan.FromMinutes(5));
+            Log += OnLog;
+        }
+
+        protected new virtual void OnStarted()
+        {
+            Logger.Info("Waiting for PortfolioFoundEvent event.");
+            PortfolioFoundEvent.WaitOne();
+
+            Logger.Info("Waiting for SecurityFound event.");
+            SecurityFoundEvent.WaitOne();
+
+            OrdersCheckTimer = new Timer(CheckOrdersElapsed, null, 0, OrdersCheckInterval.Milliseconds);
+            Series = new CandleSeries(typeof(TimeFrameCandle), Security, CandleTimeFrame)
+            {
+                From = CurrentTime - TimeSpan.FromTicks(CandleTimeFrame.Ticks * IndicatorLength)
+            };
+
+            this.WhenNewMyTrades()
+                .Do(OnNewOrderTrades)
+                .Apply(this);
+
+            this.GetCandleManager()
+                .WhenCandlesFinished(Series)
+                .Do(ProcessFinishedCandle)
+                .Apply(this);
+
+            this.GetCandleManager()
+                .WhenCandlesChanged(Series)
+                .Do(ProcessCandle)
+                .Apply(this);
+
+            this.GetCandleManager().Start(Series);
+
+            Security
+                .WhenChanged(SafeGetConnector())
+                .Do(ProcessSecurityChange)
+                .Apply(this);
+            
+            //            SafeGetConnector().RegisterMarketDepth(Security);
+
+            base.OnStarted();
+        }
+        protected new virtual void OnStopped()
+        {
+            //            SafeGetConnector().UnRegisterMarketDepth(Security);
+            if (this.GetCandleManager() != null)
+                this.GetCandleManager().Stop(Series);
+
+            base.OnStopped();
+        }
+        protected new virtual void OnReseted()
+        {
+/*            if (_area != null)
+                new ChartRemoveAreaCommand(_area).Process(this);
+            _area = null;
+*/
+            base.OnReseted();
+        }
+
+        protected void OnLog(LogMessage message)
+        {
+            if (message.Level > LogLevels.Debug)
+            {
+                Logger.InfoFormat("[{0}] {1}", message.Source, message.Message);
+            }
+        }
+        protected override void OnError(Exception error)
+        {
+            Logger.Error(error);
+            base.OnError(error);
+        }
+        protected virtual void ProcessSecurityChange()
+        {
+            if (Security.LastTrade.Time < this.StartedTime)
+            {
+                if (Security.LastTrade.Time.Minute == 0 && Security.LastTrade.Time.Second == 0)
+                {
+                    Logger.DebugFormat("Loading Trades ... {0}", Security.LastTrade.Time);
+                }
+            }
+        }
+
+        protected virtual void CheckOrdersElapsed(object stateInfo)
+        {
+            var now = this.CurrentTime;
+            foreach (var order in Orders.Where(o => o.IsMatchedEmpty()))
+            {
+                if (order.Time.Add(OrderExpirationTimeSpan) < now)
+                {
+                    CancelOrder(order);
+                }
+            }
+        }
+
+        protected virtual void ProcessCandle(Candle candle)
+        {
+        }
+
+        protected virtual void ProcessFinishedCandle(Candle candle)
+        {
+        }
+
+        protected virtual void OnNewOrderTrades(IEnumerable<MyTrade> trades)
+        {
+            foreach (var t in trades)
+            {
+                if (t.Order.Type == OrderTypes.Conditional)
+                {
+                    continue;
+                }
+
+                var direction = (t.Order.Direction == Sides.Buy) ? Sides.Sell : Sides.Buy;
+
+                var order = new Order
+                {
+                    Type = OrderTypes.Conditional,
+                    Volume = t.Order.Volume,
+                    //Price = stopLossLevel,
+                    Direction = direction,
+                    Security = t.Order.Security,
+                    Condition = new QuikOrderCondition
+                    {
+                        Type = QuikOrderConditionTypes.TakeProfitStopLimit,
+                        StopPrice =
+                            (direction == Sides.Sell)
+                                ? t.Order.Price + TakeProfitLevel
+                                : t.Order.Price - TakeProfitLevel,
+                        StopLimitPrice =
+                            (direction == Sides.Sell) ? t.Order.Price - StopLossLevel : t.Order.Price + StopLossLevel,
+                        Offset = TakeProfitOffset,
+                        IsMarketStopLimit = true,
+                        IsMarketTakeProfit = true
+                    }
+                };
+
+                RegisterOrder(order);
+            }
+
+/*
+            foreach (var t in trades)
+            {
+                new ChartDrawCommand(t.Trade.Time, new Dictionary<IChartElement, object>
+                                    {
+                                        { _area.Elements[3], t }
+                                    }).Process(this);
+            }
+*/
+        }
+    }
+}
